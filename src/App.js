a6e0df085bbdf1ac6704900bcfeb7f8df36e518e
abdessamie abdellah <abdessamie.abdellah@gmail.com>
2022-06-24 09:14:04 -0700

    First commit for tesslaWebsitePractice

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Header';
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+     <div className='App'>
+      
+       
+       <Header/>
+      <Home/>
+      
+    </div>
+  );
+}
+
+export default App;
